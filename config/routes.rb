25bfdb0ceaa1d33ee478c62ec716441e34995681
Venkata Vellanki <venkata_vellanki@yahoo.com>
2015-07-23 22:39:11 -0400

    many changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ROR::Application.routes.draw do
+  devise_for :users
+
+ root :to => 'pages#home'
+    get 'about' => 'pages#about'
+    get 'settings' => 'pages#settings'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ ROR::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+    
 
   # See how all your routes lay out with "rake routes"
 
